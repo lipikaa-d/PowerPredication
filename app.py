a40a5a8d6e1e3d2c405fd678a41a29d40cba3efb
lipikaa-d <138562600+lipikaa-d@users.noreply.github.com>
2025-06-05 15:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -73,5 +74,7 @@ def predict():
     except Exception as e:
         return f"Error: {e}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
